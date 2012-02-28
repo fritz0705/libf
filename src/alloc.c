2bf5e905e7b2ac38fb45c481f9ce1680568f8c5e
Fritz Grimpen <fritz@grimpen.net>
2012-02-28 13:13:02 +0000

    Implemented support for freestanding environments

---
+++ src/alloc.c
@@ -0,0 +1,58 @@
+/* Copyright (c) 2012 Fritz Grimpen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <f/alloc.h>
+
+static void *(*alloc_cb)(unsigned int);
+static void (*unalloc_cb)(void *);
+
+#if __STDC_HOSTED__ == 1
+#include <stdlib.h>
+alloc_cb = malloc;
+unalloc_cb = free;
+#else
+alloc_cb = NULL;
+unalloc_cb = NULL;
+#endif
+
+void *alloc(unsigned int length)
+{
+	if (alloc_cb == NULL)
+		return NULL;
+	return alloc_cb(length);
+}
+
+void unalloc(void *a)
+{
+	if (unalloc_cb == NULL)
+		return;
+	unalloc_cb(a);
+}
+
+void alloc_setup(void *(*callback)(unsigned int))
+{
+	alloc_cb = callback;
+}
+
+void unalloc_setup(void (*callback)(void *))
+{
+	unalloc_cb = callback;
+}
+
