8bb685b7dc242c06299e9824d085f33f772babdb
Fritz Grimpen <fritz@grimpen.net>
2012-03-31 21:30:42 +0000

    Implemented a simple array structure

---
+++ include/f/array.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2012 Fritz Grimpen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <f/_.h>
+
+typedef struct array *array_t;
+
+array_t array_new(size_t len);
+
+void *array_add(array_t ary, void *element);
+void array_delete(array_t ary, int offset);
+
+void *array_get(array_t ary, int offset);
+int array_find(array_t ary, void *element);
+
+void array_clean(array_t ary);
+void array_destroy(array_t ary);
+
